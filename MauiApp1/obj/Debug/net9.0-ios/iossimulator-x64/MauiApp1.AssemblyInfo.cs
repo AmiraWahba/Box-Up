6e5cf2de776b4de7281b0d7937f8f0717a7aac88
Amira Wahba <waamira7@gmail.com>
2025-02-04 05:05:28 +0200

    The Version 2/4/2025

--- MauiApp1/obj/Debug/net9.0-ios/iossimulator-x64/MauiApp1.AssemblyInfo.cs
+++ MauiApp1/obj/Debug/net9.0-ios/iossimulator-x64/MauiApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+3705946e70e812ea8b9d3842f7115fbfb05ce714")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
